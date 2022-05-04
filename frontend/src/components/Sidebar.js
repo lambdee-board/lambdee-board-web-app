96f2e313862ce30d3858d4f4fbab65ba6ba1c509
Mipplz <evilv99@gmail.com>
2022-05-05 00:06:54 +0200

    Changed sidebar from class to functional style

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 
-export class Sidebar extends Component {
-  render() {
-    return (
-      <div>Sidebar</div>
-    )
-  }
+export const Sidebar = (props) => {
+  return (
+    <div>Sidebar</div>
+  )
 }
 
 const mapStateToProps = (state) => ({})
@@ -14,7 +12,5 @@ const mapStateToProps = (state) => ({})
 const mapDispatchToProps = {}
 
 export default connect(mapStateToProps, mapDispatchToProps)(Sidebar)
-
-
 
 // code above is React snippet from 'VS Code ES7+ React/Redux/React-Native/JS snippets'
