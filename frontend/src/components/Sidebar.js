6b65fe4c892744741c84782128c09ea360d1dd25
Mateusz Drewniak <matmg24@gmail.com>
2022-05-05 09:58:26 +0200

    Remove unnecessary lines from Sidebar.js

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,16 +1,7 @@
 import React from 'react'
-import { connect } from 'react-redux'
 
 export const Sidebar = (props) => {
   return (
     <div>Sidebar</div>
   )
 }
-
-const mapStateToProps = (state) => ({})
-
-const mapDispatchToProps = {}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Sidebar)
-
-// code above is React snippet from 'VS Code ES7+ React/Redux/React-Native/JS snippets'
