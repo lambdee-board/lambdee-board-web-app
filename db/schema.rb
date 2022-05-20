a400810a753d7ff11f2809851c09fbc30f0b92a7
Michal Zaporski <focus16k@gmail.com>
2022-05-20 09:49:43 +0200

    Add colour to the board model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_17_203113) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_20_073810) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_17_203113) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "workspace_id"
+    t.string "colour", limit: 9
     t.index ["workspace_id"], name: "index_boards_on_workspace_id"
   end
 
