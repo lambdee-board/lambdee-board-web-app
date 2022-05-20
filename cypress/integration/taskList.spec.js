18029e489441ee3898db836fd574da3a709ab02f
Kuba Jadrzak <kuba.jadrzak@gmail.com>
2022-05-20 23:25:50 +0200

    Remove unnecessary code from cypress test

--- cypress/integration/taskList.spec.js
+++ cypress/integration/taskList.spec.js
@@ -18,35 +18,12 @@ describe('TaskList', () => {
         .click({ multiple: true })
       cy.get('.TaskList-new-task').should('exist')
       cy.get('.TaskList-new-task-cancel').should('exist').click({ multiple: true })
+      cy.get('.TaskList-new-task-cancel').should('not.exist')
       cy.get('.TaskList-new-task-button').should('exist').click({ multiple: true })
+      cy.get('.TaskList-new-task-button').should('not.exist')
       cy.get('.css-rhrbtd-MuiInputBase-input').should('exist').first()
         .click()
         .type('Cypress New Task')
     })
   })
-  //   it('can interact with dropdown buttons', () => {
-  //     // Workspaces
-  //     cy.get('div.MuiModal-root').should('not.exist')
-  //     cy.contains('Workspaces').click()
-  //     cy.get('div.MuiModal-root').should('exist')
-  //     cy.get('div.MuiBackdrop-root').first().click()
-
-  //     // Recent
-  //     cy.get('div.MuiModal-root').should('not.exist')
-  //     cy.contains('Recent').click()
-  //     cy.get('div.MuiModal-root').should('exist')
-  //     cy.get('div.MuiBackdrop-root').first().click()
-
-  //     // Actions
-  //     cy.get('div.MuiModal-root').should('not.exist')
-  //     cy.contains('Actions').click()
-  //     cy.get('div.MuiModal-root').should('exist')
-  //     cy.get('div.MuiBackdrop-root').first().click()
-
-//     // My account avatar
-//     cy.get('div.MuiModal-root').should('not.exist')
-//     cy.get('button.IconButton-user-avatar').click()
-//     cy.get('div.MuiModal-root').should('exist')
-//     cy.get('div.MuiBackdrop-root').first().click()
-//   })
 })
