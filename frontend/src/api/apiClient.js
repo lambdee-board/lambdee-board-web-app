e695345542fc33f75d9e0e315745d4efdcb814be
Mateusz Drewniak <matmg24@gmail.com>
2022-05-21 13:06:21 +0200

    Improve the apiClient configuration

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -6,6 +6,35 @@ const axiosClient = axios.create({
   withCredentials: true
 })
 
+if (process.env.NODE_ENV === 'development') {
+  axiosClient.interceptors.response.use((response) => {
+    console.log(
+      `
+  ${response?.config?.method?.toUpperCase()} ${response?.request?.responseURL}
+  Sent: %O
+  Received: HTTP ${response.status} %O
+      `,
+      response.config.data && JSON.parse(response.config.data) || response.config.params || null,
+      response.data,
+    )
+    return response
+  }, (error) => {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    console.error(
+      `
+  ${error?.config?.method?.toUpperCase()} ${error?.request?.responseURL}
+  Request failed with HTTP response code ${error.request.status}.
+
+  Server response:
+    ${error?.request?.response}
+      `,
+      error?.response?.data
+    )
+    return Promise.reject(error)
+  })
+}
+
 const apiClient = applyCaseMiddleware(axiosClient, {
   ignoreHeaders: true
 })
--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -2,25 +2,65 @@ import React from 'react'
 import { List, ListItem, Paper, Toolbar, InputBase, Card, ListSubheader, Typography, IconButton, Button } from '@mui/material'
 import { Box } from '@mui/system'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPencil, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faPencil, faPlus, faXmark } from '@fortawesome/free-solid-svg-icons'
 import PropTypes from 'prop-types'
 import apiClient from '../api/apiClient'
 
 import './TaskList.sass'
 
-const postTest = () => {
-
-}
-
 
 export default function TaskList(props) {
   const [visible, setVisible] = React.useState(true)
+  const listRef = React.useRef()
+  const newTaskInputRef = React.useRef()
+
+  const handleNewTaskClick = () => {
+    setVisible(!visible)
+    setTimeout(() => {
+      if (!listRef.current || !newTaskInputRef.current) return
+
+      const taskList = listRef.current
+      taskList.scrollTop = taskList.scrollHeight + 200
+      const nameInput = newTaskInputRef.current.children[0]
+      nameInput.focus()
+    }, 25)
+  }
+
+  const createNewTask = () => {
+    const nameInput = newTaskInputRef.current.children[0]
+    const newTask = {
+      name: nameInput.value,
+      listId: 1, // TODO: these values should be fetched
+      authorId: 1,
+    }
+    apiClient.post('/api/tasks', newTask)
+      .then((response) => {
+        // successful request
+      })
+      .catch((error) => {
+        // failed or rejected
+      })
+  }
+
+  const newTaskNameInputOnKey = (e) => {
+    switch (e.key) {
+    case 'Enter':
+      e.preventDefault()
+      createNewTask()
+      break
+    case 'Escape':
+      e.preventDefault()
+      setVisible(!visible)
+      break
+    }
+  }
+
   return (
     <Box className='TaskList-wrapper'>
       <Toolbar />
       <Paper className='TaskList-paper'
         elevation={5}>
-        <List className='TaskList'
+        <List ref={listRef} className='TaskList'
           subheader={<ListSubheader className='TaskList-header' >
             <Typography className='TaskList-header-text' >
               {props.title}
@@ -34,30 +74,23 @@ export default function TaskList(props) {
               {item}
             </ListItem>
           ))}
-          {!visible &&
-          <Card className='TaskList-new-task'>
+          <Card style={{ display: visible && 'none' }} className='TaskList-new-task'>
             <InputBase
+              ref={newTaskInputRef}
               className='TaskList-new-task-input'
               fullWidth
               multiline
               placeholder='Task Label'
-              onKeyPress={(e) => {
-                if (e.key === 'Enter') {
-                  e.preventDefault()
-                  apiClient.post('/api/tasks', { name: 'New Task' })
-                  setVisible(!visible)
-                }
-              }}
+              onKeyDown={newTaskNameInputOnKey}
             />
             <IconButton className='TaskList-new-task-cancel' onClick={() => setVisible(!visible)}>
-              <FontAwesomeIcon className='TaskList-new-task-cancel-icon' icon={faTrash} />
+              <FontAwesomeIcon className='TaskList-new-task-cancel-icon' icon={faXmark} />
             </IconButton>
           </Card>
-          }
         </List>
         <Box className='TaskList-new-task-wrapper'>
           {visible &&
-            <Button onClick={() => setVisible(!visible)} className='TaskList-new-task-button' color='secondary' startIcon={<FontAwesomeIcon icon={faPlus} />}>
+            <Button onClick={handleNewTaskClick} className='TaskList-new-task-button' color='secondary' startIcon={<FontAwesomeIcon icon={faPlus} />}>
               <Typography>New Task</Typography>
             </Button>
           }
