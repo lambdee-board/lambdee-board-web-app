dda3c551c46511b7b90ef24fdabdb2d14b39a74c
Kuba Jadrzak <kuba.jadrzak@gmail.com>
2022-06-01 19:24:15 +0200

    Create TaskPoints component (frontend + backend)

--- frontend/src/components/task-card-modal/TaskPoints.js
+++ frontend/src/components/task-card-modal/TaskPoints.js
@@ -19,70 +19,100 @@ import UserInfo from './UserInfo'
 
 
 function TaskPoints({ task, mutate }) {
-  // const dispatch = useDispatch()
+  const dispatch = useDispatch()
   const [editPointsVisible, setEditPointsVisible] = React.useState(false)
 
-  // const toggleEditTaskLabelButton = () => setEditTaskLabelVisible(!editTaskLabelButtonVisible)
-  // const editTaskLabelRef = React.useRef()
+  const toggleEditPointsButton = () => setEditPointsVisible(!editPointsVisible)
+  const editPointsRef = React.useRef()
 
-  // const editTaskLabelOnClick = () => {
-  //   toggleEditTaskLabelButton()
-  //   setTimeout(() => {
-  //     if (!editTaskLabelRef.current) return
+  const editPointsOnClick = () => {
+    toggleEditPointsButton()
+    setTimeout(() => {
+      if (!editPointsRef.current) return
 
-  //     const nameInput = editTaskLabelRef.current.children[0]
-  //     nameInput.focus()
-  //   }, 25)
-  // }
+      const nameInput = editPointsRef.current.children[0]
+      nameInput.focus()
+    }, 25)
+  }
 
-  // const editTaskLabel = () => {
-  //   const newLabel = editTaskLabelRef.current.children[0]
-  //   const updatedLabel = {
-  //     name: newLabel.value,
+  const editPoints = () => {
+    const newPoints = editPointsRef.current.children[0]
+    const updatedPoints = {
+      points: newPoints.value,
 
-  //   }
-  //   apiClient.put(`/api/tasks/${task.id}`, updatedLabel)
-  //     .then((response) => {
-  //       // successful request
-  //       mutate({ ...task, names: [...task?.name || [], response.data] })
-  //       toggleEditTaskLabelButton()
-  //     })
-  //     .catch((error) => {
-  //       // failed or rejected
-  //       console.log(error)
-  //       dispatch(addAlert({ severity: 'error', message: 'Something went wrong!' }))
-  //     })
-  // }
+    }
+    apiClient.put(`/api/tasks/${task.id}`, updatedPoints)
+      .then((response) => {
+        // successful request
+        mutate({ ...task, names: [...task?.name || [], response.data] })
+        toggleEditPointsButton()
+      })
+      .catch((error) => {
+        // failed or rejected
+        console.log(error)
+        dispatch(addAlert({ severity: 'error', message: 'Something went wrong!' }))
+      })
+  }
 
-  // const editTaskLabelInputOnKey = (e) => {
-  //   switch (e.key) {
-  //   case 'Enter':
-  //     e.preventDefault()
-  //     editTaskLabel()
-  //     break
-  //   case 'Escape':
-  //     e.preventDefault()
-  //     toggleEditTaskLabelButton()
-  //     break
-  //   }
-  // }
+  const editPointsInputOnKey = (e) => {
+    switch (e.key) {
+    case 'Enter':
+      e.preventDefault()
+      editPoints()
+      break
+    case 'Escape':
+      e.preventDefault()
+      toggleEditPointsButton()
+      break
+    }
+  }
   return (
     <Box className='TaskPoints'>
-      {task.points ? <Box>
+      {task.points ? (<Box>
         {!editPointsVisible ? (
-          <IconButton className='TaskPoints-button'>
-            <Avatar>{task.points}</Avatar>
+          <IconButton sx={{ p: 0, m: 0 }} onClick={editPointsOnClick} className='TaskPoints-add-button'>
+            <Avatar className='TaskPoints-avatar'>{task.points}</Avatar>
           </IconButton>
-        ) : (<Box />)
+        ) : (
+          <Box className='TaskPoints-add-button'>
+            <Avatar className='TaskPoints-avatar' alt='Add points'>
+              <InputBase
+                ref={editPointsRef}
+                className='TaskPoints-input-text'
+                onKeyDown={(e) => editPointsInputOnKey(e)}
+                onBlur={(e) => toggleEditPointsButton()}
+                inputProps={{ maxLength: 2 }}
+              />
+            </Avatar>
+          </Box>
+        )
+
         }
-      </Box> :
-        <Box className='TaskPoints-add-button'>
-          <Avatar className='TaskPoints-avatar' alt='Add points'>
-            <FontAwesomeIcon icon={faPlus} />
-          </Avatar>
-          <UserInfo userName='Add' />
+      </Box>) : (
+        <Box >
+          {!editPointsVisible ? (
+            <Box onClick={editPointsOnClick} className='TaskPoints-add-button'>
+              <Avatar className='TaskPoints-avatar' alt='Add points'>
+                <FontAwesomeIcon icon={faPlus} />
+              </Avatar>
+              <UserInfo userName='Add' />
+            </Box>
+          ) : (
+            <Box className='TaskPoints-add-button'>
+              <Avatar className='TaskPoints-avatar' alt='Add points'>
+                <InputBase
+                  ref={editPointsRef}
+                  className='TaskPoints-input-text'
+                  onKeyDown={(e) => editPointsInputOnKey(e)}
+                  onBlur={(e) => toggleEditPointsButton()}
+                  inputProps={{ maxLength: 2 }}
+                />
+              </Avatar>
+            </Box>)
+          }
+
         </Box>
-      }
+      )}
     </Box>
   )
 }
