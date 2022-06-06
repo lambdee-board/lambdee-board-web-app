51616d4521efd74807aa5b392aa0f55cd2bd9404
Mateusz Drewniak <matmg24@gmail.com>
2022-06-06 10:53:18 +0200

    Fix Task validation

--- app/models/db/task.rb
+++ app/models/db/task.rb
@@ -24,7 +24,7 @@ class DB::Task < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 80 }
   validates :description, length: { maximum: 1000 }
-  validates :points, numericality: { in: 0..99 }
+  validates :points, numericality: { in: 0..99 }, if: ->(task) { task.points.present? }
 
   class << self
     alias with_all_associations include_associations
