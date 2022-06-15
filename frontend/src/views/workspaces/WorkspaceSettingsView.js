728c25c6d455e6de7d6ba9d5ce5449fee1bf052f
Kuba Jadrzak <kuba.jadrzak@gmail.com>
2022-06-15 23:07:22 +0200

    Create basic layout

--- frontend/src/views/workspaces/WorkspaceSettingsView.js
+++ frontend/src/views/workspaces/WorkspaceSettingsView.js
@@ -1,21 +1,82 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import {
-  Typography,
   Box,
-  Card,
-  Skeleton,
-  Avatar,
-  Stack,
-  IconButton,
-  Button,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  TextField,
+  Divider,
+  Avatar
 } from '@mui/material'
+import {
+  faClipboardList,
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useParams } from 'react-router-dom'
+import useWorkspace from '../../api/useWorkspace'
+import useWorkspaceUsers  from '../../api/useWorkspaceUsers'
+import NewBoardButton from '../../components/NewBoardButton'
 import './WorkspaceSettingsView.sass'
 
 
-const WorkspaceSettings = () => {
+function BoardListItem(props) {
   return (
-    <Box className='WorkspaceSettings-wrapper'></Box>
+    <Box>
+      <ListItem>
+        <ListItemIcon>
+          {props.icon}
+        </ListItemIcon>
+        <ListItemText primary={props.label} />
+      </ListItem>
+      <Divider />
+    </Box>
+  )
+}
+function UserListItem(props) {
+  return (
+    <Box>
+      <ListItem>
+        <Avatar src={props.icon} />
+        <ListItemText primary={props.label} />
+      </ListItem>
+      <Divider />
+    </Box>
+  )
+}
+
+const WorkspaceSettings = () => {
+  const { workspaceId } = useParams()
+  const { data: workspace, mutate, isLoading, isError } = useWorkspace(workspaceId, { params: { boards: 'visible' } })
+  const { data: users, isUsersLoading, isUsersError } = useWorkspaceUsers(workspaceId)
+
+  return (
+
+    <Box className='WorkspaceSettings-wrapper'>
+      <NewBoardButton />
+      {isLoading || isError ? (
+        <Box></Box>
+      ) : (
+        <List>
+          {workspace.boards?.map((board, index) => (
+            <BoardListItem
+              key={board.name + index}
+              label={board.name}
+              icon={<FontAwesomeIcon icon={faClipboardList} color={board.colour} />}
+            />
+          ))}
+          {users?.map((user, index) => (
+            <UserListItem
+              key={user.name + index}
+              label={user.name}
+              icon={user.avatarUrl}
+            />
+          ))}
+        </List>
+
+      )}
+    </Box>
   )
 }
 
