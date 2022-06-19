63b39aaa710aca8a5e014df7b233de86caa1a723
Michal Zaporski <focus16k@gmail.com>
2022-06-19 18:34:30 +0200

    Add additional test

--- test/admin/user_importer_test.rb
+++ test/admin/user_importer_test.rb
@@ -29,6 +29,34 @@ class UserImporterTest < ActiveSupport::TestCase
     assert_nil importer.file_error_message
   end
 
+  should 'import users from csv file and deactivate them' do
+    uploaded_file = ::ActionDispatch::Http::UploadedFile.new({
+      tempfile: ::File.new(::Rails.root.join('test/support/users.csv'))
+    })
+    importer = ::UserImporter.new(uploaded_file, '1')
+    assert_difference('::DB::User.only_deleted.count', 2) do
+      importer.run
+    end
+
+    user1 = ::DB::User.with_deleted.first
+    assert_equal 'michal', user1.name
+    assert_equal 'example@example.com', user1.email
+    assert_equal 'developer', user1.role
+    assert user1.deleted?
+
+    user2 = ::DB::User.with_deleted.second
+    assert_equal 'jacob', user2.name
+    assert_equal 'guest@xd.pl', user2.email
+    assert_equal 'guest', user2.role
+    assert user2.deleted?
+
+    assert importer.full_message.include?('2 users from 4 rows have been imported!')
+    assert importer.full_message.include?('Validation failed: Email has already been taken')
+    assert importer.full_message.include?("'admins' is not a valid role")
+    assert_not importer.file_error?
+    assert_nil importer.file_error_message
+  end
+
   should 'fail if file is not a csv file' do
     uploaded_file = ::ActionDispatch::Http::UploadedFile.new({
       tempfile: ::File.new(::Rails.root.join('test/support/wrong_file.jpg'))
