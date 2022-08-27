38f166280a7f141f09cbd12255a4425a5f87d85c
KubaJadrzak <kuba.jadrzak@gmail.com>
2022-08-27 22:00:45 +0200

    Create basic WelcomeView grid layout

--- frontend/src/views/workspaces/WelcomeView.js
+++ frontend/src/views/workspaces/WelcomeView.js
@@ -2,9 +2,13 @@ import React from 'react'
 import {
   Skeleton,
   Typography,
+  Grid
 } from '@mui/material'
 
 import useCurrentUser from '../../api/useCurrentUser'
+import useBoard from '../../api/useBoard'
+import useWorkspaces from '../../api/useWorkspaces'
+import useUsers from '../../api/useUsers'
 
 import './WelcomeView.sass'
 
@@ -20,8 +24,10 @@ function WelcomeViewSkeleton() {
 
 export default function WelcomeView() {
   const { data: user, isLoading, isError } = useCurrentUser()
+  const { data: board, isBoardLoading, isBoardError } = useBoard({ id: 'recently_viewed', axiosOptions: { params: { lists: 'visible' } } })
+  const { data: workspaces,  isWorkspacesLoading, isWorkspacesError } = useWorkspaces({ limit: null })
 
-  if (isLoading || isError) return (
+  if (isLoading || isError || isBoardLoading || isBoardError || isWorkspacesError || isWorkspacesLoading) return (
     <WelcomeViewSkeleton />
   )
 
@@ -29,6 +35,38 @@ export default function WelcomeView() {
     <div className='welcomeView-wrapper'>
       <div className='welcomeView-message'>
         <Typography color='primary' fontSize={48}>Good to see you back, {user.name}!</Typography>
+      </div>
+      <div className='welcomeView-recents'>
+        <Typography color='primary' fontSize={24}>Recents</Typography>
+        <div className='welcomeView-recents-buttons'>
+          <Grid container spacing={0}
+            justifyContent='space-evenly'
+          >
+            {board?.map((recentBoard) => (
+              <Grid item xs={6} md={2} key={recentBoard.id}>
+                <div className='welcomeView-recents-button'>
+                  {recentBoard.name}
+                </div>
+              </Grid>
+            ))}
+          </Grid>
+        </div>
+      </div>
+      <div className='welcomeView-workspaces'>
+        <Typography color='primary' fontSize={24}>Workspaces</Typography>
+        <div className='welcomeView-workspaces-buttons'>
+          <Grid container spacing={0}
+            justifyContent='space-evenly'
+          >
+            {workspaces?.map((workspace) => (
+              <Grid item xs={6} md={2} key={workspace.id}>
+                <div>
+                  {workspace.name}
+                </div>
+              </Grid>
+            ))}
+          </Grid>
+        </div>
       </div>
     </div>
   )
