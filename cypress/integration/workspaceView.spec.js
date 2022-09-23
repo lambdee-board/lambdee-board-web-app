d40b26265ff1d1a9614bb18dd926e055cd3f3f9c
Mateusz Drewniak <matmg24@gmail.com>
2022-09-23 12:19:25 +0200

    Fix routing

--- cypress/integration/workspaceView.spec.js
+++ cypress/integration/workspaceView.spec.js
@@ -11,25 +11,6 @@ describe('Workspace View', () => {
     cy.get('body').first().click()
   })
 
-  // context('Sidebar', () => {
-  //   it('displays the sidebar', () => {
-  //     cy.get('div.Sidebar-wrapper').should('exist')
-  //     cy.contains('Settings')
-  //     cy.contains('Members')
-  //     cy.get('div.ListItem-board').should('exist')
-  //   })
-
-  //   it('collapses and expands', () => {
-  //     cy.get('div.Sidebar-wrapper .List-wrapper').should('be.visible')
-  //     // Click on the Sidebar collapse button
-  //     cy.get('div.Sidebar-wrapper .toggle-button').click()
-  //     cy.get('div.Sidebar-wrapper .List-wrapper').should('not.be.visible')
-
-  //     cy.get('div.Sidebar-wrapper .toggle-button').click()
-  //     cy.get('div.Sidebar-wrapper .List-wrapper').should('be.visible')
-  //   })
-  // })
-
   context('Board', () => {
     beforeEach(() => {
       cy.get('div.ListItem-board').first().click()
