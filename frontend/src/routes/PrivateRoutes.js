6ae7267c8063311370638121d59ac3ff3313ab1b
KubaJadrzak <kuba.jadrzak@gmail.com>
2022-10-19 21:54:20 +0200

    Code cleanup

--- frontend/src/routes/PrivateRoutes.js
+++ frontend/src/routes/PrivateRoutes.js
@@ -2,16 +2,12 @@ import {
   Outlet,
   Navigate
 } from 'react-router-dom'
-import PropTypes from 'prop-types'
 
 function PrivateRoutes() {
   return (
     localStorage.getItem('token') ? <Outlet /> : <Navigate to='/login' />
   )
 }
-PrivateRoutes.propTypes = {
-  children: PropTypes.object,
-  isAuthenticated: PropTypes.string,
-}
+
 
 export default PrivateRoutes
--- frontend/src/routes/PublicRoutes.js
+++ frontend/src/routes/PublicRoutes.js
@@ -2,16 +2,12 @@ import {
   Outlet,
   Navigate
 } from 'react-router-dom'
-import PropTypes from 'prop-types'
 
 function PublicRoutes() {
   return (
     !localStorage.getItem('token') ? <Outlet /> : <Navigate to='/' />
   )
 }
-PublicRoutes.propTypes = {
-  children: PropTypes.object,
-  isAuthenticated: PropTypes.string,
-}
+
 
 export default PublicRoutes
