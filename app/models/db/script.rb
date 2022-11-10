618254f91e6a89eecb32b46dd5d77168114bee71
miczmu <m.zmudzinski@espago.com>
2022-11-10 20:31:16 +0100

    small relations fix

--- app/models/db/script.rb
+++ app/models/db/script.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class DB::Script < ApplicationRecord
-  has_many :script_run, dependent: :destroy
-  has_many :callback_script, dependent: :destroy
-  has_many :ui_script, dependent: :destroy
+  has_many :script_runs, dependent: :destroy
+  has_many :callback_scripts, dependent: :destroy
+  has_many :ui_scripts, dependent: :destroy
 end
