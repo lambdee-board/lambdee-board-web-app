cdb9308b405a70f55eca1206b48c33fbf42e015a
Michal Zaporski <focus16k@gmail.com>
2023-01-18 23:01:52 +0100

    refactoring

--- app/models/db/ui_script_trigger.rb
+++ app/models/db/ui_script_trigger.rb
@@ -4,6 +4,8 @@
 # causes a particular script to run
 # when some predefined event happens.
 class ::DB::UiScriptTrigger < ::ApplicationRecord
+  include ::ScriptTriggerSearchable
+
   ALLOWED_SUBJECT_CLASSES = ::Set['DB::Workspace', 'DB::Board', 'DB::Task']
 
   belongs_to :subject, polymorphic: true, optional: true
@@ -14,10 +16,6 @@ class ::DB::UiScriptTrigger < ::ApplicationRecord
 
   before_save :set_private_for_global
 
-  scope :with_author, ->(author) { where(author: author) }
-  scope :not_private, -> { where(private: false) }
-  scope :for_record, ->(record) { where(subject: record) }
-  scope :for_model, ->(model, scope) { where(subject_type: model.to_s).where(subject_id: nil).where(scope: scope) }
   scope :global, ->(author) { with_author(author).where(subject_type: nil) }
 
   default_scope { order(id: :desc) }
@@ -29,18 +27,6 @@ class ::DB::UiScriptTrigger < ::ApplicationRecord
   validates :subject_type, inclusion: { in: ALLOWED_SUBJECT_CLASSES }, allow_nil: true
   validates :colour, length: { minimum: 7, maximum: 9 }, allow_blank: true
   validates :text, length: { maximum: 100 }
-
-  class << self
-    # @param record [ApplicationRecord]
-    # @param user [DB::User]
-    def regarding_record_and_user(record, user)
-      query = for_record(record)
-      record.class::AVAILABLE_SCOPES.each do |scope_name|
-        query = query.or(for_model(record.class, record.public_send(scope_name)))
-      end
-      query = query.merge(not_private.or(with_author(user)))
-    end
-  end
 
   # @param subject_id [String, Integer]
   # @return [ApplicationRecord, nil]
