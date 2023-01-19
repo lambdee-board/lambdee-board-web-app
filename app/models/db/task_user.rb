7eba6c316979cb777053f64a9aa5fec6329e2a08
Michal Zaporski <focus16k@gmail.com>
2023-01-19 20:10:22 +0100

    Allow to add script trigger on TaskUser

--- app/models/db/task_user.rb
+++ app/models/db/task_user.rb
@@ -3,6 +3,12 @@
 # Represents a many-to-many
 # relation between `Task` and `User`.
 class DB::TaskUser < ::ApplicationRecord
+  AVAILABLE_SCOPES = ::Set[:workspace, :board, :list, :task]
+
   belongs_to :task, class_name: 'DB::Task'
   belongs_to :user, class_name: 'DB::User'
+
+  delegate :list, to: :task
+  delegate :board, to: :list
+  delegate :workspace, to: :board
 end
